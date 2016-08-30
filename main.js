403f0d867ddb6a9e16ab4357b83d4e1cd003da8d
samme <samme@github.invalid>
2016-08-29 17:35:31 -0700

    Example: use a state object

--- main.js
+++ main.js
@@ -1,110 +1,111 @@
 'use strict';
 
-var game = new Phaser.Game("90", "85", Phaser.WEBGL, 'game', {preload: preload, create: create, update: update, render: render});
-
-var ball;
-var newY;
-
-var playerPaddle;
-var playerScore = 0;
-var playerScoreText;
-var playerName = "Player";
-
-//Just doing this for the lulz
-var LazerBeam;
-
-var enemyPaddle;
-
-function preload() {
-	game.add.plugin(Phaser.Plugin.Debug);
-	game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-	game.scale.pageAlignHorizontally = true;
-	game.scale.pageAlignVertically = true;
-	game.stage.backgroundColor = '#eee';
-	game.load.image('paddle', 'assets/paddle.png');
-	game.load.image('ball', 'assets/ball.png');
+var state = {
+    
+    preload: function (){
+    	this.add.plugin(Phaser.Plugin.Debug);
+    	this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+    	this.scale.pageAlignHorizontally = true;
+    	this.scale.pageAlignVertically = true;
+    	this.stage.backgroundColor = '#eee';
+    	this.load.image('paddle', 'assets/paddle.png');
+    	this.load.image('ball', 'assets/ball.png');
 	
-	game.load.script("LazerBeam", "assets/LazerBeam.js");
-}
-function create() {
-	game.physics.startSystem(Phaser.Physics.ARCADE);
-	game.physics.arcade.checkCollision.left = false;
-	game.physics.arcade.checkCollision.right = false;
-	
-	LazerBeam = game.add.filter("LazerBeam", game.world.width, game.world.height, 0.5);
-	
-	playerPaddle = game.add.sprite(5, game.world.height * 0.5, 'paddle');
-	playerPaddle.anchor.set(0.5,1);
-	game.physics.enable(playerPaddle, Phaser.Physics.ARCADE);
-	playerPaddle.body.immovable = true;
-	
-	playerScoreText = game.add.text(0, 0, playerName + " score: " + playerScore);
-	
-	enemyPaddle = game.add.sprite(game.world.width-10, game.world.height * 0.5, 'paddle');
-	enemyPaddle.anchor.set(0.5, 1);
-	game.physics.enable(enemyPaddle, Phaser.Physics.ARCADE);
-	enemyPaddle.body.immovable = true;
-	enemyPaddle.body.collideWorldBounds = true;
-	
-	ballSpawn();
-}
-function update() {
-	game.physics.arcade.collide(ball, playerPaddle, ballHitPaddle);
-	game.physics.arcade.collide(ball, enemyPaddle, ballHitPaddle);
-	playerPaddle.y = game.input.y || game.world.height*0.5;
-	
-	if (Math.ceil(ball.x) % 5 === 0) {
-		newY = ball.body.angle + ball.body.velocity.y * 2;
-	}
-	
-	if (enemyPaddle.y > newY + 20) {
-		enemyPaddle.body.velocity.y = -200;
-	}
-	if (enemyPaddle.y < newY - 20) {
-		enemyPaddle.body.velocity.y = 200;
-	}
-}
+    	this.load.script("LazerBeam", "assets/LazerBeam.js");
+    },
 
-function render() {
-	// http://phaser.io/docs/2.4.4/Phaser.Utils.Debug.html
-	//game.debug.spriteInfo(playerPaddle, 32, 32);
-	//game.debug.body(playerPaddle);
-	//game.debug.inputInfo();
-	game.debug.text(newY + " " + ball.x, 50, 50);
-	//game.debug.pointer(game.input.activePointer);
-}
+    create: function (){
+        this.newY = 0;
+        this.playerScore = 0;
+        this.playerScoreText = "";
+        this.playerName = "Player";
 
-function ballLeaveScreen() {
-	if (ball.x >= game.world.width) {
-		playerScore++;
-	} else {
-		playerScore--;
-	}
+    	this.physics.startSystem(Phaser.Physics.ARCADE);
+    	this.physics.arcade.checkCollision.left = false;
+    	this.physics.arcade.checkCollision.right = false;
 	
-	playerScoreText.text = playerName + " score: " + playerScore;
-	ballReset();
-}
-function ballHitPaddle(ball, paddle) {
-	//TODO (Maybe): Modify the bounce. The code below leads to some weird issues
-	//ball.body.velocity.x = -(ball.speed);
-	//ball.body.velocity.y = (paddle.body.velocity > 0) ? paddle.body.velocity.y * 10 : 150;
+    	this.LazerBeam = this.add.filter("LazerBeam", this.world.width, this.world.height, 0.5);
 	
-	game.add.tween(ball).to({tint: 0xFFF}, 100, Phaser.Easing.Cubic.In, true, 0, 0, true);
-}
+    	this.playerPaddle = this.add.sprite(5, this.world.height * 0.5, 'paddle');
+    	this.playerPaddle.anchor.set(0.5,1);
+    	this.physics.enable(this.playerPaddle, Phaser.Physics.ARCADE);
+    	this.playerPaddle.body.immovable = true;
+	
+    	this.playerScoreText = this.add.text(0, 0, this.playerName + " score: " + this.playerScore);
+	
+    	this.enemyPaddle = this.add.sprite(this.world.width-10, this.world.height * 0.5, 'paddle');
+    	this.enemyPaddle.anchor.set(0.5, 1);
+    	this.physics.enable(this.enemyPaddle, Phaser.Physics.ARCADE);
+    	this.enemyPaddle.body.immovable = true;
+    	this.enemyPaddle.body.collideWorldBounds = true;
+        
+        this.input.keyboard.addKey(Phaser.KeyCode.R).onDown.add(this.state.restart, this.state);
+	
+    	this.ballSpawn();
+    },
 
-function ballSpawn() {
-	ball = game.add.sprite(50, 50, 'ball');
-	game.physics.enable(ball, Phaser.Physics.ARCADE);
-	ball.body.collideWorldBounds = true;
-	ball.checkWorldBounds = true;
-	ball.body.bounce.set(1);
-	ball.events.onOutOfBounds.add(ballLeaveScreen, this);
+    update: function (){
+    	this.physics.arcade.collide(this.ball, this.playerPaddle, this.ballHitPaddle, null, this);
+    	this.physics.arcade.collide(this.ball, this.enemyPaddle, this.ballHitPaddle, null, this);
+    	this.playerPaddle.y = this.input.y || this.world.height*0.5;
+	
+    	if (Math.ceil(this.ball.x) % 5 === 0) {
+    		this.newY = this.ball.body.angle + this.ball.body.velocity.y * 2;
+    	}
+	
+    	if (this.enemyPaddle.y > this.newY + 20) {
+    		this.enemyPaddle.body.velocity.y = -200;
+    	}
+    	if (this.enemyPaddle.y < this.newY - 20) {
+    		this.enemyPaddle.body.velocity.y = 200;
+    	}
+    },
 
-	ballReset();
-	console.log("Ball spawned");
-}
+    render: function (){
+    	// http://phaser.io/docs/2.4.4/Phaser.Utils.Debug.html
+    	//game.debug.spriteInfo(playerPaddle, 32, 32);
+    	//game.debug.body(playerPaddle);
+    	//game.debug.inputInfo();
+    	this.game.debug.text(this.newY + " " + this.ball.x, 50, 50);
+    	//game.debug.pointer(game.input.activePointer);
+    },
 
-function ballReset () {
-	ball.reset(50, 50);
-	ball.body.velocity.set(250, 250);
-}
+    ballLeaveScreen: function (){
+    	if (this.ball.x >= this.world.width) {
+    		this.playerScore++;
+    	} else {
+    		this.playerScore--;
+    	}
+	
+    	this.playerScoreText.text = this.playerName + " score: " + this.playerScore;
+    	this.ballReset();
+    },
+
+    ballHitPaddle: function (ball, paddle){
+    	//TODO (Maybe): Modify the bounce. The code below leads to some weird issues
+    	//ball.body.velocity.x = -(ball.speed);
+    	//ball.body.velocity.y = (paddle.body.velocity > 0) ? paddle.body.velocity.y * 10 : 150;
+	
+    	this.add.tween(ball).to({tint: 0xFFF}, 100, Phaser.Easing.Cubic.In, true, 0, 0, true);
+    },
+
+    ballSpawn: function (){
+    	this.ball = this.add.sprite(50, 50, 'ball');
+    	this.physics.enable(this.ball, Phaser.Physics.ARCADE);
+    	this.ball.body.collideWorldBounds = true;
+    	this.ball.checkWorldBounds = true;
+    	this.ball.body.bounce.set(1);
+    	this.ball.events.onOutOfBounds.add(this.ballLeaveScreen, this);
+
+    	this.ballReset();
+    	console.log("Ball spawned");
+    },
+
+    ballReset: function (){
+    	this.ball.reset(50, 50);
+    	this.ball.body.velocity.set(250, 250);
+    }
+    
+};
+
+var game = new Phaser.Game("90", "85", Phaser.WEBGL, 'game', state);
